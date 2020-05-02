e28d7075f4ae85d5990deb8c07c35222265c2aba
Akshay8797 <60914748+Akshay8797@users.noreply.github.com>
2020-05-02 21:59:29 +0530

    HrService

--- Interview_Tracking_System/src/main/java/com/spring/controller/HrController.java
+++ Interview_Tracking_System/src/main/java/com/spring/controller/HrController.java
@@ -3,21 +3,23 @@ package com.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spring.json.ITS_TBL_Hrpanel_Json;
+import com.spring.json.ITS_TBL_Interview_Schedule;
 import com.spring.service.HrService;
 
 @RestController
-@RequestMapping("/its")
+@RequestMapping("/")
+@CrossOrigin(origins="*")
 public class HrController {
 	@Autowired
 	private HrService hrService;
 	
 	@GetMapping("/hr")
-	public List<ITS_TBL_Hrpanel_Json> getAllHrPanel() {
-		return hrService.getAllHrPanel();
+	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate() {
+		return hrService.getAllHrCandidate();	
 	}
 }
