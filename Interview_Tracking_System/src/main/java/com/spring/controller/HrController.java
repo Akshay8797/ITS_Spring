d095d3c37e20eb52596fc33043c2a016207bfeff
Akshay8797 <60914748+Akshay8797@users.noreply.github.com>
2020-05-07 00:18:37 +0530

    HrController

--- Interview_Tracking_System/src/main/java/com/spring/controller/HrController.java
+++ Interview_Tracking_System/src/main/java/com/spring/controller/HrController.java
@@ -3,9 +3,14 @@ package com.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.json.ITS_TBL_Interview_Schedule;
@@ -13,13 +18,24 @@ import com.spring.service.HrService;
 
 @RestController
 @RequestMapping("/")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class HrController {
 	@Autowired
 	private HrService hrService;
-	
+
 	@GetMapping("/hr")
 	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate() {
-		return hrService.getAllHrCandidate();	
+		return hrService.getAllHrCandidate();
+	}
+	
+	@PostMapping(value = "/hr/{interviewId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String giveHRRating(@RequestBody ITS_TBL_Interview_Schedule its_tbl_interview_schedule,
+			@PathVariable(value = "interviewId") String id) {
+		return hrService.giveHRRating(its_tbl_interview_schedule, id);
+	}
+
+	@GetMapping("/result")
+	public List<ITS_TBL_Interview_Schedule> getResult() {
+		return hrService.getResult();
 	}
 }
