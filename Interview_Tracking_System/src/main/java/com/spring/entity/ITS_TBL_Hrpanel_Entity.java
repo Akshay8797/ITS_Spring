6fdc543586a39f94d26e92d4831cd2ef08789eac
Akshay8797 <akshaykumar7301@gmail.com>
2020-05-07 21:09:32 +0530

    Everything Updated, Don't Modify

--- Interview_Tracking_System/src/main/java/com/spring/entity/ITS_TBL_Hrpanel_Entity.java
+++ Interview_Tracking_System/src/main/java/com/spring/entity/ITS_TBL_Hrpanel_Entity.java
@@ -1,7 +1,6 @@
 package com.spring.entity;
 
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -9,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
--- Interview_Tracking_System/src/main/java/com/spring/json/ITS_TBL_Candidate.java
+++ Interview_Tracking_System/src/main/java/com/spring/json/ITS_TBL_Candidate.java
@@ -6,7 +6,7 @@ import com.spring.entity.ITS_TBL_Interview_Schedule_Entity;
 
 public class ITS_TBL_Candidate {
 
-	private long candidateId; 
+	private long candidateId;
 	private String primarySkills;
 	private String secondarySkills;
 	private int experience;
@@ -16,12 +16,11 @@ public class ITS_TBL_Candidate {
 	private String location;
 	private int shareDetails;
 	private List<ITS_TBL_Interview_Schedule_Entity> interviewScheduleList;
-	
-	public ITS_TBL_Candidate()
-	{
+
+	public ITS_TBL_Candidate() {
 		super();
 	}
-	
+
 	public ITS_TBL_Candidate(String primarySkills, String secondarySkills, int experience, String qualification,
 			String designation, int noticePeriod, String location, int shareDetails) {
 		super();
@@ -34,10 +33,7 @@ public class ITS_TBL_Candidate {
 		this.location = location;
 		this.shareDetails = shareDetails;
 	}
-	
-	
-	
-	
+
 	public ITS_TBL_Candidate(long candidateId, String primarySkills, String secondarySkills, int experience,
 			String qualification, String designation, int noticePeriod, String location, int shareDetails) {
 		super();
@@ -51,66 +47,86 @@ public class ITS_TBL_Candidate {
 		this.location = location;
 		this.shareDetails = shareDetails;
 		this.interviewScheduleList = interviewScheduleList;
-		
+
 	}
+
 	public List<ITS_TBL_Interview_Schedule_Entity> getInterviewScheduleList() {
 		return interviewScheduleList;
 	}
+
 	public void setInterviewScheduleList(List<ITS_TBL_Interview_Schedule_Entity> interviewScheduleList) {
 		this.interviewScheduleList = interviewScheduleList;
 	}
+
 	public long getCandidateId() {
 		return candidateId;
 	}
+
 	public void setCandidateId(long candidateId) {
 		this.candidateId = candidateId;
 	}
+
 	public String getPrimarySkills() {
 		return primarySkills;
 	}
+
 	public void setPrimarySkills(String primarySkills) {
 		this.primarySkills = primarySkills;
 	}
+
 	public String getSecondarySkills() {
 		return secondarySkills;
 	}
+
 	public void setSecondarySkills(String secondarySkills) {
 		this.secondarySkills = secondarySkills;
 	}
+
 	public int getExperience() {
 		return experience;
 	}
+
 	public void setExperience(int experience) {
 		this.experience = experience;
 	}
+
 	public String getQualification() {
 		return qualification;
 	}
+
 	public void setQualification(String qualification) {
 		this.qualification = qualification;
 	}
+
 	public String getDesignation() {
 		return designation;
 	}
+
 	public void setDesignation(String designation) {
 		this.designation = designation;
 	}
+
 	public int getNoticePeriod() {
 		return noticePeriod;
 	}
+
 	public void setNoticePeriod(int noticePeriod) {
 		this.noticePeriod = noticePeriod;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public int getShareDetails() {
 		return shareDetails;
 	}
+
 	public void setShareDetails(int shareDetails) {
 		this.shareDetails = shareDetails;
-	}	
+	}
 }
