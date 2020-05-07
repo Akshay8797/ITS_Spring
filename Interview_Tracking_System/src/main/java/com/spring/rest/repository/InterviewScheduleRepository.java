179cb362d486a8805e6f224dc382003161d5537d
anishkumar321 <60914890+anishkumar321@users.noreply.github.com>
2020-05-07 19:56:22 +0530

    Update InterviewScheduleRepository.java

--- Interview_Tracking_System/src/main/java/com/spring/rest/repository/InterviewScheduleRepository.java
+++ Interview_Tracking_System/src/main/java/com/spring/rest/repository/InterviewScheduleRepository.java
@@ -8,7 +8,17 @@ import com.spring.entity.ITS_TBL_Interview_Schedule_Entity;
 @Component
 public interface InterviewScheduleRepository extends JpaRepository<ITS_TBL_Interview_Schedule_Entity, Long>{
 
-	ITS_TBL_Interview_Schedule_Entity findByInterviewId(long interviewId);
+	//ITS_TBL_Interview_Schedule_Entity findByInterviewId(long interviewId);
+	List<ITS_TBL_Interview_Schedule_Entity> findByTechRatingGreaterThan(Float tr);
+
+	ITS_TBL_Interview_Schedule_Entity findByInterviewId(String interviewId);
+
+	List<ITS_TBL_Interview_Schedule_Entity> findByEmpHRRatingIsNull();
+	List<ITS_TBL_Interview_Schedule_Entity> findByShareResultIsNotNull();
+	List<ITS_TBL_Interview_Schedule_Entity> findByInterviewTimeIsNotNull();
+	List<ITS_TBL_Interview_Schedule_Entity> findByTechRatingIsNotNull();
+	@Query(value="SELECT * FROM ITS_TBL_Interview_Schedule i WHERE i.interview_time is NULL AND i.tech_rating is NULL" ,nativeQuery = true)
+	List<ITS_TBL_Interview_Schedule_Entity> findByInterviewTimeIsNullAndfindByTechRatingIsNull();
 	
 	
 }
