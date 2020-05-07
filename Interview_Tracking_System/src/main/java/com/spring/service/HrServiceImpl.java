4fae0e79cf130977057749bc65393f0edc426284
sibu38 <60917900+sibu38@users.noreply.github.com>
2020-05-07 17:17:58 +0530

    Hr Rating margin set to 3f
    previously margin was 2.5f

--- Interview_Tracking_System/src/main/java/com/spring/service/HrServiceImpl.java
+++ Interview_Tracking_System/src/main/java/com/spring/service/HrServiceImpl.java
@@ -27,12 +27,12 @@ public class HrServiceImpl implements HrService {
 		ITS_TBL_Interview_Schedule_Entity its_tbl_interview_schedule_entity = hrcandidateRepo.findById(Long.valueOf(id))
 				.get();
 		if (its_tbl_interview_schedule_entity.getEmpHRRating() == (Float) null
-				&& its_tbl_interview_schedule_entity.getTechRating() >= 2.5f) {
+				&& its_tbl_interview_schedule_entity.getTechRating() >= 3f) {
 			its_tbl_interview_schedule_entity.setEmpHRRating(its_tbl_interview_schedule.getEmpHRRating());
 			its_tbl_interview_schedule_entity = hrcandidateRepo.save(its_tbl_interview_schedule_entity);
 			return "{\"Result\" :\" Success\"}";
 		} else if (its_tbl_interview_schedule_entity.getEmpHRRating() == (Float) null
-				&& its_tbl_interview_schedule_entity.getTechRating() < 2.5f) {
+				&& its_tbl_interview_schedule_entity.getTechRating() < 3f) {
 			return "{\"Result\" :\" Candididate doesn't qualify the Tech Round\"}";
 		} else
 			return "{\"Result\" :\" Candididate is already assigned with HR Rating\"}";
