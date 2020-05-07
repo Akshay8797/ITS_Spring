b4b74dbdd397d697ea1389b0e5453564f829a854
anishkumar321 <60914890+anishkumar321@users.noreply.github.com>
2020-05-07 17:48:55 +0530

    Update ITS_TBL_Interview_Schedule.java

--- Interview_Tracking_System/src/main/java/com/spring/json/ITS_TBL_Interview_Schedule.java
+++ Interview_Tracking_System/src/main/java/com/spring/json/ITS_TBL_Interview_Schedule.java
@@ -8,7 +8,6 @@ public class ITS_TBL_Interview_Schedule {
 	private String subject;
 	private LocalDate interviewDate;
 	private LocalTime interviewTime;
-<<<<<<< HEAD
 	private Float techRating;
 	private LocalDate empHRInterviewDate;
 	private LocalDate empHRInterviewTime;
@@ -22,18 +21,6 @@ public class ITS_TBL_Interview_Schedule {
 	public ITS_TBL_Interview_Schedule(long interviewId, String subject, LocalDate interviewDate,
 		LocalTime interviewTime, Float techRating, LocalDate empHRInterviewDate, LocalDate empHRInterviewTime,
 		Float empHRRating, String result, Integer shareResult) {
-=======
-	private float techRating;
-	private LocalDate empHRInterviewDate;
-	private LocalTime empHRInterviewTime;
-	private float empHRRating;
-	private String result;
-	private int shareResult;
-
-	public ITS_TBL_Interview_Schedule(long interviewId, String subject, LocalDate interviewDate,
-			LocalTime interviewTime, Float techRating, LocalDate empHRInterviewDate, LocalTime empHRInterviewTime,
-			Float empHRRating, String result, int shareResult) {
->>>>>>> 3af5cd6d0bcf7e9b9c56c1dc220303c856b2f1e7
 		super();
 		this.interviewId = interviewId;
 		this.subject = subject;
@@ -46,71 +33,49 @@ public class ITS_TBL_Interview_Schedule {
 		this.result = result;
 		this.shareResult = shareResult;
 	}
-<<<<<<< HEAD
 	
 	public ITS_TBL_Interview_Schedule(String subject, Float techRating) {
 		super();
 		this.subject = subject;
 		this.techRating = techRating;
 	}
-=======
->>>>>>> 3af5cd6d0bcf7e9b9c56c1dc220303c856b2f1e7
 
 	public long getInterviewId() {
 		return interviewId;
 	}
-
 	public void setInterviewId(long interviewId) {
 		this.interviewId = interviewId;
 	}
-
 	public String getSubject() {
 		return subject;
 	}
-
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-
 	public LocalDate getInterviewDate() {
 		return interviewDate;
 	}
-
 	public void setInterviewDate(LocalDate interviewDate) {
 		this.interviewDate = interviewDate;
 	}
-
 	public LocalTime getInterviewTime() {
 		return interviewTime;
 	}
-
 	public void setInterviewTime(LocalTime interviewTime) {
 		this.interviewTime = interviewTime;
 	}
-<<<<<<< HEAD
 	public Float getTechRating() {
 		return techRating;
 	}
 	public void setTechRating(Float techRating) {
-=======
-
-	public float getTechRating() {
-		return techRating;
-	}
-
-	public void setTechRating(float techRating) {
->>>>>>> 3af5cd6d0bcf7e9b9c56c1dc220303c856b2f1e7
 		this.techRating = techRating;
 	}
-
 	public LocalDate getEmpHRInterviewDate() {
 		return empHRInterviewDate;
 	}
-
 	public void setEmpHRInterviewDate(LocalDate empHRInterviewDate) {
 		this.empHRInterviewDate = empHRInterviewDate;
 	}
-<<<<<<< HEAD
 	public LocalDate getEmpHRInterviewTime() {
 		return empHRInterviewTime;
 	}
@@ -121,52 +86,25 @@ public class ITS_TBL_Interview_Schedule {
 		return empHRRating;
 	}
 	public void setEmpHRRating(Float empHRRating) {
-=======
-
-	public LocalTime getEmpHRInterviewTime() {
-		return empHRInterviewTime;
-	}
-
-	public void setEmpHRInterviewTime(LocalTime empHRInterviewTime) {
-		this.empHRInterviewTime = empHRInterviewTime;
-	}
-
-	public float getEmpHRRating() {
-		return empHRRating;
-	}
-
-	public void setEmpHRRating(float empHRRating) {
->>>>>>> 3af5cd6d0bcf7e9b9c56c1dc220303c856b2f1e7
 		this.empHRRating = empHRRating;
 	}
-
 	public String getResult() {
 		return result;
 	}
-
 	public void setResult(String result) {
 		this.result = result;
 	}
-<<<<<<< HEAD
 	public Integer getShareResult() {
 		return shareResult;
 	}
 	public void setShareResult(Integer shareResult) {
-=======
-
-	public int getShareResult() {
-		return shareResult;
-	}
-
-	public void setShareResult(int shareResult) {
->>>>>>> 3af5cd6d0bcf7e9b9c56c1dc220303c856b2f1e7
 		this.shareResult = shareResult;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		ITS_TBL_Interview_Schedule its_schedule = (ITS_TBL_Interview_Schedule) obj;
-		if (this.interviewId == (its_schedule.interviewId))
+		if (this.interviewId==(its_schedule.interviewId))
 			return true;
 		return false;
 	}
