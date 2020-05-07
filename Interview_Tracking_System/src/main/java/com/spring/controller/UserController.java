360fa1dfbd28474dd9788fc0168b66b297c4e164
SandeepSubudhi <60914834+SandeepSubudhi@users.noreply.github.com>
2020-05-07 08:30:17 +0530

    Updated Login,Logout

--- Interview_Tracking_System/src/main/java/com/spring/controller/UserController.java
+++ Interview_Tracking_System/src/main/java/com/spring/controller/UserController.java
@@ -3,6 +3,7 @@ package com.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +16,28 @@ import com.spring.service.UserService;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins="*")
 public class UserController {
-
+	
 	@Autowired
 	private UserService userService;
+	private String sessionId = null;
 
+	@PostMapping(value="/login", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String loginUser(@RequestBody ITS_TBL_USER_CREDENTIALS user) {
+		return userService.login(user);
+	}
+	/*@PostMapping(value="/logout", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String logoutUser(@RequestHeader(name="auth-token") String authToken) {
+		return userService.logout(authToken); 
+	}*/
 	@PostMapping(value = "/resetpassword", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String resetpassword(@RequestBody ITS_TBL_USER_CREDENTIALS user,
 			@RequestParam("newpassword") String newpassword) {
 		return userService.resetpassword(user, newpassword);
+	}
+	@GetMapping("/logout")
+	public String logoutAdmin() {
+		return userService.logout(sessionId);
 	}
 }
