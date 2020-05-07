24ef2a04c95af91d9f618446ccc619b3c890e362
sibu38 <60917900+sibu38@users.noreply.github.com>
2020-05-07 20:11:36 +0530

    Update HrService.java

--- Interview_Tracking_System/src/main/java/com/spring/service/HrService.java
+++ Interview_Tracking_System/src/main/java/com/spring/service/HrService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.spring.json.ITS_TBL_Interview_Schedule;
 
 public interface HrService {
-	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate();
-
-	public String giveHRRating(ITS_TBL_Interview_Schedule its_tbl_interview_schedule, String id);
+	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate(); 
+	
+	public String giveHRRating (ITS_TBL_Interview_Schedule its_tbl_interview_schedule, String id);
 
 	public List<ITS_TBL_Interview_Schedule> getResult();
 }
