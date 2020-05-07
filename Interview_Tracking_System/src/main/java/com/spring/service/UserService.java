35a47529ad223096fe8043771671bdf66d3fe162
SandeepSubudhi <60914834+SandeepSubudhi@users.noreply.github.com>
2020-05-07 08:24:38 +0530

    Update UserService.java
    updated login logout

--- Interview_Tracking_System/src/main/java/com/spring/service/UserService.java
+++ Interview_Tracking_System/src/main/java/com/spring/service/UserService.java
@@ -4,6 +4,10 @@ import com.spring.json.ITS_TBL_USER_CREDENTIALS;
 
 public interface UserService {
 
+	public String login(ITS_TBL_USER_CREDENTIALS user);
+
+	public String logout(String authToken);
+	
 	public String resetpassword(ITS_TBL_USER_CREDENTIALS user, String newpassword);
 
 }
