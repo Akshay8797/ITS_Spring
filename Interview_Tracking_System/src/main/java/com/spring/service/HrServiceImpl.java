4f1a68c62dbb1148d5119e2e94b27060129c381a
sibu38 <60917900+sibu38@users.noreply.github.com>
2020-05-07 20:10:22 +0530

    Update HrServiceImpl.java

--- Interview_Tracking_System/src/main/java/com/spring/service/HrServiceImpl.java
+++ Interview_Tracking_System/src/main/java/com/spring/service/HrServiceImpl.java
@@ -14,11 +14,17 @@ import com.spring.utils.InterviewScheduleUtils;
 public class HrServiceImpl implements HrService {
 	@Autowired
 	private InterviewScheduleRepository hrcandidateRepo;
-
+/*
 	@Override
 	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate() {
 		List<ITS_TBL_Interview_Schedule_Entity> hrcandidateEntityList = hrcandidateRepo
 				.findByTechRatingGreaterThan((Float) 3f);
+		return InterviewScheduleUtils.convertScheduleEntityListToScheduleList(hrcandidateEntityList);
+	}
+*/	
+	@Override
+	public List<ITS_TBL_Interview_Schedule> getAllHrCandidate() {
+		List<ITS_TBL_Interview_Schedule_Entity> hrcandidateEntityList = hrcandidateRepo.findByHRInterviewTimeIsNullAndfindByTechRatingIsGreaterThan3();
 		return InterviewScheduleUtils.convertScheduleEntityListToScheduleList(hrcandidateEntityList);
 	}
 
@@ -27,12 +33,12 @@ public class HrServiceImpl implements HrService {
 		ITS_TBL_Interview_Schedule_Entity its_tbl_interview_schedule_entity = hrcandidateRepo.findById(Long.valueOf(id))
 				.get();
 		if (its_tbl_interview_schedule_entity.getEmpHRRating() == (Float) null
-				&& its_tbl_interview_schedule_entity.getTechRating() >= 2.5f) {
+				&& its_tbl_interview_schedule_entity.getTechRating() >= 3f) {
 			its_tbl_interview_schedule_entity.setEmpHRRating(its_tbl_interview_schedule.getEmpHRRating());
 			its_tbl_interview_schedule_entity = hrcandidateRepo.save(its_tbl_interview_schedule_entity);
 			return "{\"Result\" :\" Success\"}";
 		} else if (its_tbl_interview_schedule_entity.getEmpHRRating() == (Float) null
-				&& its_tbl_interview_schedule_entity.getTechRating() < 2.5f) {
+				&& its_tbl_interview_schedule_entity.getTechRating() < 3f) {
 			return "{\"Result\" :\" Candididate doesn't qualify the Tech Round\"}";
 		} else
 			return "{\"Result\" :\" Candididate is already assigned with HR Rating\"}";
