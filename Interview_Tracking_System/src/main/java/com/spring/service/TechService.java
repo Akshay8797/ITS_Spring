2344930b7f3206ff4caded4f930226209442f5cf
anishkumar321 <60914890+anishkumar321@users.noreply.github.com>
2020-05-07 19:57:58 +0530

    Update TechService.java

--- Interview_Tracking_System/src/main/java/com/spring/service/TechService.java
+++ Interview_Tracking_System/src/main/java/com/spring/service/TechService.java
@@ -6,4 +6,7 @@ import com.spring.entity.ITS_TBL_Techpanel_Entity;
 
 public interface TechService {
 	public List<ITS_TBL_Techpanel_Entity> getAllTechPanel();  
+	public List<ITS_TBL_Interview_Schedule> getAllInterviewCandidates();
+	public String giveTechRating(ITS_TBL_Interview_Schedule its_tbl_interview_schedule, String id);
+	public List<ITS_TBL_Interview_Schedule> getFinalResultsForTech();
 }
